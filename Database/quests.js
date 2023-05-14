1c839e6ee4e3606eb1a7cf279c18ffccf7135892
Marcus Rasmussen <mnsr@hotmail.dk>
2023-05-15 01:04:51 +0200

    Added more poster variations

--- Database/quests.js
+++ Database/quests.js
@@ -1,25 +1,102 @@
 // quests.js
 window.quests = [
     {
-      title: 'The Cursed Amulet',
-      description: 'The village of Delmaria is plagued by darkness ever since the Cursed Amulet was stolen from the Temple of Light. ',
-      difficulty: 'Easy',
-      reward: '500 gold',
-      posterType: 'poster2'
-    },
-    {
-      title: 'The Cursed Amulet',
-      description: 'The village of Delmaria is plagued by darkness ever since the Cursed Amulet was stolen from the Temple of Light. The amulet\'s power has corrupted the land, causing crops to wither and villagers to fall ill. Journey through treacherous catacombs, face deadly traps, and battle undead guardians to reclaim the amulet and restore light to Delmaria.',
-      difficulty: 'Easy',
-      reward: '500 gold',
+      title: 'Fill in',
+      description: 'Fill in',
+      difficulty: 'Fill in',
+      reward: 'Fill in',
       posterType: 'poster1'
     },
     {
-      title: 'The Cursed Amulet',
+      title: 'Fill in',
+      description: 'Fill in',
+      difficulty: 'Fill in',
+      reward: 'Fill in',
+      posterType: 'poster2'
+    },
+    {
+      title: 'Fill in',
+      description: 'Fill in',
+      difficulty: 'Fill in',
+      reward: 'Fill in',
+      posterType: 'poster3'
+    },
+    {
+      title: 'Fill in',
+      description: 'Fill in',
+      difficulty: 'Fill in',
+      reward: 'Fill in',
+      posterType: 'poster4'
+    },
+    {
+      title: 'The Cursed Amulet Amulet',
       description: 'The village of Delmaria is plagued by darkness ever since the Cursed Amulet was stolen from the Temple of Light. The amulet\'s power has corrupted the land, causing crops to wither and villagers to fall ill. Journey through treacherous catacombs, face deadly traps, and battle undead guardians to reclaim the amulet and restore light to Delmaria.',
       difficulty: 'Easy',
-      reward: '500 gold',
-      posterType: 'poster3'
+      reward: '50000000 gold',
+      posterType: 'poster5'
+    },
+    {
+      title: 'Fill in',
+      description: 'Fill in',
+      difficulty: 'Fill in',
+      reward: 'Fill in',
+      posterType: 'poster6'
+    },
+    {
+      title: 'Fill in',
+      description: 'Fill in',
+      difficulty: 'Fill in',
+      reward: 'Fill in',
+      posterType: 'poster7'
+    },
+    {
+      title: 'Fill in',
+      description: 'Fill in',
+      difficulty: 'Fill in',
+      reward: 'Fill in',
+      posterType: 'poster8'
+    },
+    {
+      title: 'Fill in',
+      description: 'Fill in',
+      difficulty: 'Fill in',
+      reward: 'Fill in',
+      posterType: 'poster9'
+    },
+    {
+      title: 'Fill in',
+      description: 'Fill in',
+      difficulty: 'Fill in',
+      reward: 'Fill in',
+      posterType: 'poster10'
+    },
+    {
+      title: 'Fill in',
+      description: 'Fill in',
+      difficulty: 'Fill in',
+      reward: 'Fill in',
+      posterType: 'poster11'
+    },
+    {
+      title: 'Fill in',
+      description: 'Fill in',
+      difficulty: 'Fill in',
+      reward: 'Fill in',
+      posterType: 'poster12'
+    },
+    {
+      title: 'Fill in',
+      description: 'Fill in',
+      difficulty: 'Fill in',
+      reward: 'Fill in',
+      posterType: 'poster13'
+    },
+    {
+      title: 'Fill in',
+      description: 'Fill in',
+      difficulty: 'Fill in',
+      reward: 'Fill in',
+      posterType: 'poster14'
     },
     // Add more quests as needed
   ];
