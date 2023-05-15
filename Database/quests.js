78cb846b18e7adc404e411d1f069dd69d946bcd8
Marcus Rasmussen <mnsr@hotmail.dk>
2023-05-15 10:18:11 +0200

    More poster formatting 6/14

--- Database/quests.js
+++ Database/quests.js
@@ -1,45 +1,45 @@
 // quests.js
 window.quests = [
     {
-      title: 'Fill in',
-      description: 'Fill in',
-      difficulty: 'Fill in',
-      reward: 'Fill in',
+      title: 'The Secret of the Whispering Caves',
+      description: 'The village of Delmaria is plagued by darkness ever since the Cursed Amulet was stolen from the Temple of Light. The amulet\'s power has corrupted the land, causing crops to wither and villagers to fall ill. Journey through treacherous catacombs, face deadly traps',
+      difficulty: 'S rank',
+      reward: '10000 bucks',
       posterType: 'poster1'
     },
     {
-      title: 'Fill in',
-      description: 'Fill in',
-      difficulty: 'Fill in',
-      reward: 'Fill in',
+      title: 'The Lost Artifact',
+      description: 'The village of Delmaria is plagued by darkness ever since the Cursed Amulet was stolen from the Temple of Light. The amulet\'s power has corrupted the land, causing crops to wither and villagers to fall ill.',
+      difficulty: 'Insanium',
+      reward: '5 hookers',
       posterType: 'poster2'
     },
     {
-      title: 'Fill in',
-      description: 'Fill in',
-      difficulty: 'Fill in',
-      reward: 'Fill in',
+      title: 'The Secret of the Whispering Caves',
+      description: 'The village of Delmaria is plagued by darkness ever since the Cursed Amulet was stolen from the Temple of Light. The amulet\'s power has corrupted the land, causing crops to wither and villagers to fall ill. Journey through treacherous catacombs, face deadly traps, and battle undead guardians to reclaim the amulet and restore light to Delmaria.light to Delmaria.',
+      difficulty: 'Unknown',
+      reward: '10 Rubies',
       posterType: 'poster3'
     },
     {
-      title: 'Fill in',
-      description: 'Fill in',
-      difficulty: 'Fill in',
-      reward: 'Fill in',
+      title: 'The Journey to the Shattered Realm',
+      description: 'The village of Delmaria is plagued by darkness ever since the Cursed Amulet was stolen from the Temple of Light. The amulet\'s power has corrupted the land, causing crops to wither and villagers to fall ill. Journey through treacherous catacombs, face deadly traps, and battle undead guardians to reclaim the amulet and restore light to Delmaria.light to Delmaria.light to Delmaria.light to',
+      difficulty: 'Nutjob',
+      reward: 'Staff of the Magi',
       posterType: 'poster4'
     },
     {
-      title: 'The Cursed Amulet Amulet',
-      description: 'The village of Delmaria is plagued by darkness ever since the Cursed Amulet was stolen from the Temple of Light. The amulet\'s power has corrupted the land, causing crops to wither and villagers to fall ill. Journey through treacherous catacombs, face deadly traps, and battle undead guardians to reclaim the amulet and restore light to Delmaria.',
+      title: 'The Curse of the Forbidden',
+      description: 'The village of Delmaria is plagued by darkness ever since the Cursed Amulet was stolen from the Temple of Light. The amulet\'s power has corrupted the land, causing crops to wither and villagers to fall ill. Journey through treacherous catacombs, face deadly traps, and battle undead guardians to reclaim the amulet and restore light to Delmaria.light to Delmaria.light to Delmaria.light to',
       difficulty: 'Easy',
       reward: '50000000 gold',
       posterType: 'poster5'
     },
     {
-      title: 'Fill in',
-      description: 'Fill in',
-      difficulty: 'Fill in',
-      reward: 'Fill in',
+      title: 'Fixed the poster',
+      description: 'The village of Delmaria is plagued by darkness ever since the Cursed Amulet was stolen from the Temple of Light. The amulet\'s power has corrupted the land, causing crops to wither and villagers to fall ill. Journey through treacherous',
+      difficulty: 'Very Hard',
+      reward: '10000 Platinum',
       posterType: 'poster6'
     },
     {
