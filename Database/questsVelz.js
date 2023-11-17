5d798ab864179d882fbc2bf93073039d9b6c7517
Lockinlove <lassefletcher@gmail.com>
2023-11-17 16:31:33 +0100

    quest

--- Database/questsVelz.js
+++ Database/questsVelz.js
@@ -18,52 +18,86 @@ window.questsVelz = [
     type: 'Civil unrest',
     description: 'A bunch of common folk in a small village has sent in report of a few houses completely missing in their community and beg for help to resolve the issue and has sent in much more money than they should',
     difficulty: 'Rank 2',
-    reward: '5 gold if the villagers are just confused, 30 if there is an actual problem',
+    reward: '5 gold if the villagers are just confused, 75 if there is an actual problem',
     posterType: 'poster5',
     x: 259,
     y: 60
     },
+
     {
     title: 'Temporary staff requested',
     type: 'Manual labor',
     description: 'A shop in Velz has requsted some extra hands for a big event they are holding',
     difficulty: 'Rank 1',
-    reward: '2 Gold per participant',
+    reward: '4 Gold per participant',
     posterType: 'poster6',
     x: 1125,
     y: 537
     },
+
     {
     title: 'Need help moving a lot of boxes',
     type: 'Manual labor',
      description: 'A shop in the city has requested help moving all the merchandise from the front gate to a warehouse further into the city',
      difficulty: 'Rank 1',
-     reward: '2 Gold per participant',
+     reward: '4 Gold per participant',
      posterType: 'poster6',
      x: 730,
     y: 467
     },
+
     {
      title: 'No contact with Bask village',
       type: 'Investigation and solution',
       description: 'Recently a lot of people have complained about missing shipments, deals or the like from 1 farm and no one has dared to get close because of a "looming presence" Figure out what is going on and deal with it accordingly',
       difficulty: 'Rank 3',
-      reward: '35 Gold',
+      reward: '105 Gold',
       seal: 'Images/Posters/Seals/GuildSeal.png',
       posterType: 'poster1',
       x: 680,
       y: 50
-      },
-      {
-      title: 'Missing in the woods',
-      type: 'Investigation',
-      description: 'Recently a lot of people have gone missing in a forest on the outskirts of Velz and little to no traces have been found on them missing people or where they have disapeared to but the few leads we have, have all been found around the same area in the forest.',
-      difficulty: 'Rank 2',
-      reward: '20 gold + 10 gold for each rescued person',
-      seal: 'Images/Posters/Seals/GuildSeal.png',
-      posterType: 'poster3',
-      x: 77,
-      y: 315
-      },           
+    },
+
+  {
+    title: 'Turn me back',
+    type: 'Transmutation',
+    description: 'Meet me at the alleyway just under the plateu',
+    difficulty: 'Rank 3',
+    reward: 'Magical favors',
+    posterType: 'poster8',
+    x: 860,
+    y: 270
+  },
+
+   {
+    title: 'Killing of Wolves',
+    type: 'Hunt',
+    description: 'Our farm has been dealing with a lot of wolves harming our people and cattle and we need the adventures help to get rid of them',
+    difficulty: 'Rank 2',
+    reward: '100G',
+    posterType: 'poster3',
+    x: 20,
+    y: 670
+   },
+  {
+    title: 'My child got taken',
+    type: 'Recovery and threat elimination',
+    description: 'When out gathering herbs and flower for some food my child Peter got taken by something and we dont have anyone on the farm that can fight, please help',
+    difficulty: 'Rank 1',
+    reward: '60G',
+    posterType: 'poster1',
+    x: 448,
+    y: 702
+   },
+   {
+    title: 'Wagon robbed',
+    type: 'Find and recover',
+    description: 'While travelling towards the inner city of Velz a wagon was jumped and although no one was killed, all the good were stolen by what seemed like Goblins.',
+    difficulty: 'Rank 2',
+    reward: '40G + whatever you find that doesnt belong to the merchant',
+    posterType: 'poster9',
+    x: 237,
+    y: 245
+   },
   ];
 // Add more quests as needed
